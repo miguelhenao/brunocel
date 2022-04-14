a2f177e1de63432e5c1bfd819247072087aa8043
miguelhenao <miguel.henao@utp.edu.co>
2022-04-14 02:04:16 -0500

    Add config for MatDialogOptions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { httpInterceptorProviders } from './helpers/http-interceptors';
 import { AppRoutingModule } from './app-routing.module';
 import { RootStoreModule } from './root-store/root-store.module';
+import { DialogSize } from './helpers/enums/dialog-size';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,7 +25,13 @@ import { RootStoreModule } from './root-store/root-store.module';
     StoreRouterConnectingModule.forRoot(),
     StoreDevtoolsModule.instrument()
   ],
-  providers: [httpInterceptorProviders],
+  providers: [
+    httpInterceptorProviders,
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: { autoFocus: false, disableClose: true, width: DialogSize.XSmall, hasBackdrop: true }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
